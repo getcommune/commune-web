4a3432ca7583b96df01fbcbb4046bae964430009
arch997 <archibongc4@gmail.com>
2023-12-09 23:50:02 +0100

    feat: add account deletion request page

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import AboutUs from "./components/AboutUs/index.vue";
 import KeyOffering from "./components/KeyOffering/index.vue";
 import HowItWorks from "./components/HowItWorks/index.vue";
 import ContactUs from "./components/ContactUs/index.vue";
+import DeleteAccount from "./components/DeleteAccount/index.vue";
 import Desktop from "./components/WaitingListModal/Desktop.vue";
 import Mobile from "./components/WaitingListModal/Mobile.vue";
 import Docs from "./components/Docs/index.vue";
@@ -19,10 +20,15 @@ export default defineComponent({
   directives: {},
   setup() {
     const docsPage = ref(false);
+    const deleteAccountPage = ref(false);
 
     onBeforeMount(() => {
       docsPage.value =
         /^https?:\/\/(?:localhost|(?:\d+\.\d+\.\d+.\d+)):\d+\/docs\/?$/.test(
+          location.href
+        );
+        deleteAccountPage.value =
+        /^https?:\/\/(?:localhost|(?:\d+\.\d+\.\d+.\d+)):\d+\/delete-account\/?$/.test(
           location.href
         );
 
@@ -47,7 +53,7 @@ export default defineComponent({
         state.value.mounted = true;
       });
     });
-    return { theme, docsPage };
+    return { theme, docsPage, deleteAccountPage };
   },
   components: {
     Header,
@@ -59,12 +65,14 @@ export default defineComponent({
     Desktop,
     Mobile,
     Docs,
+    DeleteAccount,
   },
 });
 </script>
 
 <template>
   <Docs v-if="docsPage" />
+  <DeleteAccount v-if="deleteAccountPage"/>
 
   <div
     v-else
